dde9cf86bc930ee3602025d7824aae1a23747452
Keren Zhou <kerenzhou@fb.com>
2018-07-03 11:23:30 -0700

    Init samples

---
+++ INSTALL-ANDROID.md
@@ -0,0 +1,90 @@
+## Build NNVM/TVM on macOS
+
+llvm
+
+```
+$ brew install llvm
+```
+
+tvm
+
+https://docs.tvm.ai/install/index.html
+
+```
+git clone --recursive https://github.com/dmlc/tvm
+export PYTHONPATH=/path/to/tvm/python:/path/to/tvm/topi/python:${PYTHONPATH}
+cd tvm
+mkdir build
+cp cmake/config.cmake build  # change configurations before make
+make -j4
+```
+
+### Build Android RPC
+
+maven
+
+```
+$ brew install maven
+```
+
+gradle
+
+https://docs.gradle.org/current/userguide/build_environment.html#sec:gradle_configuration_properties
+
+```
+$ brew install gradle # touch gradle.properties and configure build environment
+```
+
+ndk-bundle
+
+```
+sdkmanger ndk-bundle
+```
+
+.bashrc
+
+```
+export PATH=${ANDROID_HOME}/platform-tools:${PATH}
+export PATH=${ANDROID_HOME}/ndk-bundle:${PATH}
+```
+
+Android Standalone Toolchain
+
+```
+$${ANDROID_HOME}/ndk-bundle/build/tools/make-standalone-toolchain.sh --platform=android-28 --use-llvm --arch=arm64 --install-dir=/path/to/tvm/cc
+$ export TVM_NDK_CC=/path/to/tvm/cc/bin/aarch64-linux-android-g++
+```
+
+tvm4j
+
+```
+make jvmpkg
+make jvminstall
+```
+
+android_rpc/app/src/main/jni/config.mk
+
+https://github.com/dmlc/tvm/tree/master/apps/android_rpc
+
+```
+APP_ABI = arm64-v8a
+
+APP_PLATFORM = android-28
+
+# whether enable OpenCL during compile
+USE_OPENCL = 1
+
+# the additional include headers you want to add, e.g., SDK_PATH/adrenosdk/Development/Inc
+ADD_C_INCLUDES = /path/to/opencl/header
+
+# the additional link libs you want to add, e.g., ANDROID_LIB_PATH/libOpenCL.so
+ADD_LDLIBS = /path/to/libOpenCL.so
+```
+
+build android_rpc app
+
+```
+$ gradle clean build
+$ ./dev_tools/gen_keystore.sh
+$ ./dev_tools/sign_apk.sh
+```
---
+++ samples/android_rpc_onnx.py
@@ -0,0 +1,83 @@
+"""Testcode for Android RPC onnx model.
+
+To use it, start a rpc proxy with "python -m tvm.exec.rpc_proxy".
+And configure the proxy host field as commented.
+"""
+
+import os
+import sys
+from time import time
+
+import numpy as np
+import onnx
+import tvm
+from tvm.contrib import graph_runtime, util, ndk, rpc
+import nnvm.frontend
+import nnvm.compiler
+
+# Set to be address of tvm proxy.
+proxy_host = os.environ["TVM_ANDROID_RPC_PROXY_HOST"]
+proxy_port = 9090
+key = "android"
+
+# Change target configuration.
+# Run `adb shell cat /proc/cpuinfo` to find the arch.
+arch = "arm64"
+target = "llvm -target=%s-linux-android" % arch
+
+def test_onnx_model(tgt):
+    if tgt == "gpu":
+        # Mobile GPU
+        target = "opencl"
+        target_host = "llvm -target=%s-linux-android" % arch
+        ctx = remote.cl(0)
+    else:
+        # Mobile CPU
+        target = "llvm -target=%s-linux-android" % arch
+        target_host = None
+        ctx = remote.cpu(0)
+    data_shape = ()
+    onnx_graph = onnx.load("xxx.onnx")
+    sym, params = nnvm.frontend.from_onnx(onnx_graph)
+    opt_level = 0
+    input_name = "data"
+
+    print("Build Graph...")
+    with nnvm.compiler.build_config(opt_level=opt_level, add_pass=None):
+        graph, lib, params = nnvm.compiler.build(sym, target, {input_name: data_shape}, params=params, target_host=target_host)
+
+    dtype = np.float32
+    org_img = np.random(data_shape)
+    img = np.asarray(org_img).astype(np.float32).copy()
+    img /= 255
+
+    # connect to the proxy
+    print('RPC Connecting...')
+    remote = rpc.connect(proxy_host, proxy_port, key=key)
+    print('RPC Connected')
+
+    print('DEPLOY: Shared Library Uploading...')
+    so_name = model_name + "aarch64.so"
+    temp = util.tempdir()
+    path_so = temp.relpath(so_name)
+    lib.export_library(path_so, ndk.create_shared)
+
+    # run on remote device
+    remote.upload(path_so)
+    rlib = remote.load_module(so_name)
+    input_data = tvm.nd.array(img.astype(dtype), ctx)
+    rmodule = graph_runtime.create(graph, rlib, ctx)
+    rmodule.set_input(input_name, input_data)
+    rmodule.set_input(**params)
+    
+    print('Execute')
+    rmodule.run()
+
+    print('Benchmark')
+    num_iter = 100
+    ftimer = rmodule.module.time_evaluator("run", ctx, num_iter)
+    prof_res = ftimer()
+    print(prof_res)
+
+if __name__ == "__main__":
+    test_onnx_model("cpu")
