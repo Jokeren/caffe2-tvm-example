633a37cfc30b1b4890413af16d45263e41674d48
Keren Zhou <kerenzhou@fb.com>
2018-07-05 10:29:40 -0700

    Fix bugs

--- samples/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Specify the proxy host
-export TVM_ANDROID_RPC_PROXY_HOST=0.0.0.0
-
-# Specify the standalone Android C++ compiler
-export TVM_NDK_CC=/Users/kerenzhou/Codes/android-toolchain-arm64/bin/aarch64-linux-android-g++
-
-python $1
